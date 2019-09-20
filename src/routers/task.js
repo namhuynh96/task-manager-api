f2aedd6205d8e2eab758b2ef23bbcd66ecfd0d98
Nam Huynh <huynhchinam96@gmail.com>
2019-09-20 20:05:41 +0300

    Send status code 404 if cannot find task when uploading task image

--- src/routers/task.js
+++ src/routers/task.js
@@ -33,10 +33,14 @@ const upload = multer({
 
 router.post('/tasks/:id/image', auth, upload.single('image'), async (req, res) => {
     const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-    const task = await Task.findById(req.params.id)
-    task.image = buffer
-    await task.save()
-    res.status(201).send()
+    try {
+        const task = await Task.findById(req.params.id)
+        task.image = buffer
+        await task.save()
+        res.status(201).send()
+    } catch (e) {
+        res.status(404).send()
+    }
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
